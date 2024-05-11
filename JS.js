5469cd3d6a9ed5141a5e47ea28e57718f5d41bab
Akejyo <987383393@qq.com>
2024-05-12 04:00:43 +0800

    add img change animation

--- JS.js
+++ JS.js
@@ -2,14 +2,34 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 var particles = [];
-
-var img = new Image();
+var imgs = [
+    "https://raw.githubusercontent.com/Akejyo/Arknights_partical_animation/main/avatar.jpg",
+    "https://raw.githubusercontent.com/Akejyo/imageForBlog/master/xi_gua_ka.jpg",
+]
+var particleCount = 0;
 const blockSize = 10;
 const randomRatio = 0.3;
-img.crossOrigin = "anonymous";
-img.src = "https://raw.githubusercontent.com/Akejyo/Arknights_partical_animation/main/avatar.jpg";
+const imgSize = 900
+var imgIndex = 0;
 
-img.onload = () => {
+var listItems = document.getElementsByClassName("listItem");
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener('mouseenter', function() {
+        if (i !== imgIndex) {
+            imgIndex = i;
+            var img = new Image();
+            img.crossOrigin = "anonymous";
+            img.src = imgs[i];
+            img.width = imgSize;
+            img.height = imgSize;
+            img.onload = function() {
+                convertImageToParticles(img);
+            }
+        }
+    })
+}
+
+function convertImageToParticles(img) {
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
@@ -25,6 +45,8 @@ img.onload = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var blockX = Math.ceil(canvas.width / blockSize);
     var blockY = Math.ceil(canvas.height / blockSize);
+
+    var newParticles = [];
     for (var y = 0; y < blockY; y++) {
         for (var x = 0; x < blockX; x++) {
             var sum = 0;
@@ -36,9 +58,34 @@ img.onload = () => {
             }
             var average = Math.floor(sum / (blockSize * blockSize));
             if (average > 128) {
-                particles.push(new Particles((x + Math.random() * randomRatio) * 7, (y + Math.random() * randomRatio) * 7));
+                newParticles.push(new Particle((x + Math.random() * randomRatio) * 7, (y + Math.random() * randomRatio) * 7));
             }
         }
+    }
+
+    for (particle of particles)
+        particle.needed = false;
+    particles.sort(() => Math.random() - 0.5);
+    for (var i = 0; i < newParticles.length; i++) {
+        if (i >= particleCount) {
+            particles.push(newParticles[i]);
+        } else {
+            particles[i].needed = true;
+            particles[i].originalX = newParticles[i].originalX;
+            particles[i].originalY = newParticles[i].originalY;
+        }
+    }
+    particleCount = newParticles.length;
+}
+
+window.onload = function() { //load the first image
+    var img = new Image();
+    img.crossOrigin = "anonymous";
+    img.src = imgs[0];
+    img.width = imgSize;
+    img.height = imgSize;
+    img.onload = function() {
+        convertImageToParticles(img);
     }
 }
 
@@ -47,31 +94,35 @@ var mouseY = null;
 var forceRatio = 170;
 var dSpeedMax = 3;
 
-function Particles(x, y) {
-    this.x = Math.random() * window.innerWidth;
-    this.y = Math.random() * window.innerHeight;
+function Particle(x, y) {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
     this.originalX = x;
     this.originalY = y;
-    this.targetX = x;
-    this.targetY = y;
     this.size = 3.5;
     this.speedX = 0;
     this.speedY = 0;
     this.alpha = 1 - Math.random() * randomRatio;
     this.alphaNow = 0;
+    this.needed = true; //if the particle is needed in the new image
 }
 
-Particles.prototype.update = function() {
+Particle.prototype.update = function() {
     this.x += this.speedX;
     this.y += this.speedY;
 }
 
-Particles.prototype.draw = function() {
+Particle.prototype.draw = function() {
     ctx.fillStyle = `rgba(173, 216, 230, ${this.alphaNow + 0.005 > this.alpha ? this.alpha : this.alphaNow += 0.005})`;
     ctx.fillRect(this.x, this.y, this.size, this.size);
 }
 
-canvas.addEventListener('mousemove', function(e) {
+Particle.prototype.disappear = function() {
+    ctx.fillStyle = `rgba(173, 216, 230, ${this.alphaNow - 0.005 < 0 ? 0 : this.alphaNow -= 0.005})`;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+}
+
+window.addEventListener('mousemove', function(e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
 });
@@ -79,7 +130,6 @@ canvas.addEventListener('mousemove', function(e) {
 animateParticles = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (particle of particles) {
-
         var dToOriginalX = particle.x - particle.originalX;
         var dToOriginalY = particle.y - particle.originalY;
         particle.speedX = -dToOriginalX / 30;
@@ -96,8 +146,17 @@ animateParticles = () => {
             particle.speedX += dSpeedX < 0 ? Math.max(dSpeedX, -dSpeedMax) : Math.min(dSpeedX, dSpeedMax);
             particle.speedY += dSpeedY < 0 ? Math.max(dSpeedY, -dSpeedMax) : Math.min(dSpeedY, dSpeedMax);
         }
-        particle.update();
-        particle.draw();
+        if (particle.needed) {
+            particle.update();
+            particle.draw();
+        } else {
+            particle.speedX = 0;
+            particle.speedY = 0;
+            particle.disappear();
+            if (particle.alphaNow <= 0) {
+                particles.splice(particles.indexOf(particle), 1);
+            }
+        }
     }
 
     requestAnimationFrame(animateParticles);
